e675ddfa3d2179fbf3c87cbbffa263b2417af99b
Iris Sanchez <sanchezorts.iris@gmail.com>
2025-05-23 23:30:05 +0200

    feat(RomanNumerals) & test(RomanNumeralsTest): refactor with loop

--- src/main/java/romannumerals/RomanNumerals.java
+++ src/main/java/romannumerals/RomanNumerals.java
@@ -3,22 +3,24 @@ package romannumerals;
 public class RomanNumerals {
     public String convert(int number) {
         StringBuilder stringBuilder = new StringBuilder();
-        if(number > 5){
-            int remainder = number - 5;
-            stringBuilder.append("V");
-            for (int i = 0; i < remainder; i++) {
-                stringBuilder.append("I");
-            }
-        } else if(number >= 5-1){
-            int remainder = 5 - number;
-            for (int i = 0; i < remainder; i++) {
-                stringBuilder.append("I");
-            }
-            stringBuilder .append("V");
-
-        } else {
-            for (int i = 0; i < number; i++) {
-                stringBuilder.append("I");
+        while(true) {
+            if (number >= 4) {
+                int remainder = number - 5;
+                if(remainder > 0){
+                    stringBuilder.append("V");
+                    number = remainder;
+                } else{
+                    for (int i = 0; i < -remainder; i++) {
+                        stringBuilder.append("I");
+                    }
+                    stringBuilder.append("V");
+                    break;
+                }
+            } else {
+                for (int i = 0; i < number; i++) {
+                    stringBuilder.append("I");
+                }
+                break;
             }
         }
         return stringBuilder.toString();
